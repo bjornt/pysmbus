1344e1929005da7c7003be1b88db0be989a7693d
Bjorn Tillenius <bjorn@tillenius.me>
2015-08-30 20:41:31 +0300

    Fix setup.py to prepare for pypi upload.

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="pysmbus",
     version="0.1",
-    description="Pure Python implemetation of the I2C SMBus protocol.",
+    description="Pure Python implementation of the I2C SMBus protocol.",
+    long_description=readme,
     py_modules=["smbus"],
     include_package_data=True,
     zip_safe=True,
+    url="https://github.com/bjornt/pysmbus",
+    license="LGPL v2",
+    author="Bjorn Tillenius",
+    author_email="bjorn@tillenius.me",
     )
